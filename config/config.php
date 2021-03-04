b181825aac78e6991869f40686be11397c57cc68
mayquanxi <mayquanxi@gmail.com>
2021-03-04 10:16:16 +0700

    edit

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+$CONFIG = array (
+  'passwordsalt' => 'schaNYsguCIld3CL4MtmuDsJ+FOWUW',
+  'secret' => 'FJL82/0bsLzjXJZkU06TL+jaAr9cS4l0qGqlu3HU3TL/1d/J',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'pgsql',
+  'version' => '21.0.0.18',
+  'overwrite.cli.url' => 'http://localhost',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'bk4f7era6ym3iz3yv15v3qdrjurhvk',
+  'installed' => true,
+);
